c3114f1b25f94579cf543b69b9c1de14c5a3d7ec
Lukas Gabriel <lukasgabriel@pm.me>
2021-03-09 23:38:48 +0100

    Add Card and Deck classes

--- deck.py
+++ deck.py
@@ -1,37 +1,83 @@
 # deck.py
 
+
 import toml
 
+class Card:
+    def __init__(self, c_type: str, c_color: int, ruleset: dict = toml.load("rules_official.toml")):
+        self.deck_ref = ruleset["deck"]
+        self.points_ref = ruleset["points"]
 
-deck_ref = toml.load("rules_official.toml")["deck"]
+        self.c_type = c_type
+        self.c_color = c_color
 
-assert (
-    deck_ref["n_cards"]
-    == (deck_ref["n_colors"] * deck_ref["n_cards_per_c"])
-    + deck_ref["n_wilds"]
-    + deck_ref["n_wildfours"]
-)
-assert (
-    deck_ref["n_cards_per_c"]
-    == deck_ref["n_zeros_per_c"]
-    + (deck_ref["n_1to9_per_c"] * 9)
-    + deck_ref["n_skip_per_c"]
-    + deck_ref["n_draw2_per_c"]
-    + deck_ref["n_reverse_per_c"]
-)
+        try:
+            assert self.c_color in range(self.deck_ref["n_colors"]) # 4 Colors: 0, 1, 2, 3.
+        except AssertionError:
+            print("Invalid ruleset or card.")
 
-assert deck_ref['n_cards'] <= 256
+
+
+class Deck:
+    def __init__(self, ruleset: dict = toml.load("rules_official.toml")):
+        self.deck_ref = ruleset["deck"]
+        self.game_ref = ruleset["game"]
+
+        self.deck = []  # Working deck
+        self.pile = []  # Discard pile
+
+        try: 
+            self.assert_ruleset()
+        except AssertionError:
+            print("Invalid ruleset.")
+
+        for i in (range(self.deck_ref['n_zeros_per_c'] * self.deck_ref['n_colors'])):
+            deck.append(("0", format(counter, '02x')))
+            counter += 1
+        
+        pass
+    def shuffle(self):
+        pass
+    def draw(self):
+        pass
+    def discard(self):
+        pass
+    def assert_ruleset(self):
+        assert (
+            self.deck_ref["n_cards"]
+            == (self.deck_ref["n_colors"] * self.deck_ref["n_cards_per_c"])
+            + self.deck_ref["n_wilds"]
+            + self.deck_ref["n_wildfours"]
+        )
+        assert (
+            self.deck_ref["n_cards_per_c"]
+            == self.deck_ref["n_zeros_per_c"]
+            + (self.deck_ref["n_1to9_per_c"] * 9)
+            + self.deck_ref["n_skip_per_c"]
+            + self.deck_ref["n_draw2_per_c"]
+            + self.deck_ref["n_reverse_per_c"]
+        )
+
+        assert self.deck_ref['n_cards'] <= 256
+
+
+""" my_deck = Deck()
+
+
 
 #while counter := 0 < deck['n_cards']:
 counter = 0
 deck = []
 
-for _ in (range(deck_ref['n_zeros_per_c'] * deck_ref['n_colors'])):
+for _ in (range(self.deck_ref['n_zeros_per_c'] * self.deck_ref['n_colors'])):
     deck.append({"0":format(counter, '02x')})
     counter += 1
+
+
 for i in range(0, 9):
-    for _ in range(deck_ref["n_1to9_per_c"]):
-        for c in deck_ref["n_colors"]:
+    for _ in range(self.deck_ref["n_1to9_per_c"]):
+        for c in self.deck_ref["n_colors"]: """
             
 
-print(deck)
+
+print(Deck().deck_ref)
