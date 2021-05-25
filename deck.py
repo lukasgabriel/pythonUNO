838bd390b25d1c67db71ba939bb69156bbb7f174
Lukas Gabriel <lukasgabriel@pm.me>
2021-05-25 07:50:30 +0200

    Add str and repr methods, deck shuffling and building

--- deck.py
+++ deck.py
@@ -1,64 +1,131 @@
 # deck.py
 
-
+from math import floor
+from random import random
 import toml
 
-class Card:
-    def __init__(self, c_type: str, c_color: int, ruleset: dict = toml.load("rules_official.toml")):
+
+class Card():
+    def __init__(self, c_type: str, c_color: int, c_value: int, c_itera: int, ruleset: dict = toml.load("rules_official.toml")):
+
         self.deck_ref = ruleset["deck"]
+        self.game_ref = ruleset["game"]
         self.points_ref = ruleset["points"]
 
         self.c_type = c_type
         self.c_color = c_color
+        self.c_value = c_value
+        self.c_itera = c_itera
 
+        self.assert_ruleset()
+
+        self.desc = self.make_desc()
+
+
+    def assert_ruleset(self):
         try:
-            assert self.c_color in range(self.deck_ref["n_colors"]) # 4 Colors: 0, 1, 2, 3.
+            assert self.c_color in range(0, self.deck_ref["n_colors"]) # 4 Colors: 0, 1, 2, 3.
+            assert self.c_type in self.deck_ref["types"]
+            assert self.c_value in range(0, 9) or self.c_value in self.points_ref.values()
         except AssertionError:
             print("Invalid ruleset or card.")
 
+    def __str__(self):
+        if self.c_type in ["1to9", "zero"]:
+            name = self.c_value
+        else: 
+            name = {"wild": "W", "wild4": "F", "draw2": "D", "reverse": "R", "skip": "S"}[self.c_type]
+        color = ["R", "Y", "G", "B"][self.c_color]
+        return f"{color}{name}{self.c_itera}"
+
+    def make_desc(self):
+        pass
+
+    def __repr__(self):
+        return f"Card('{self.c_type}', {self.c_color}, {self.c_value}, {self.c_itera})"
 
 
-class Deck:
-    def __init__(self, ruleset: dict = toml.load("rules_official.toml")):
+class Deck():
+    def __init__(self, seed = None, ruleset: dict = toml.load("rules_official.toml")):
+
         self.deck_ref = ruleset["deck"]
         self.game_ref = ruleset["game"]
+        self.points_ref = ruleset["points"]
 
         self.deck = []  # Working deck
         self.pile = []  # Discard pile
 
-        try: 
-            self.assert_ruleset()
+        self.assert_ruleset()
+        self.build()
+
+    def build(self):
+        for color in range(self.deck_ref['n_colors']):
+
+            for itera in range(self.deck_ref['n_zeros_per_c']):
+                self.deck.append(Card(c_type = "zero", c_color = color, c_value = 0, c_itera = itera))
+
+            for itera in range(self.deck_ref['n_1to9_per_c']):
+                for i in range(1, 9):
+                    self.deck.append(Card(c_type = "1to9", c_color = color, c_value = i, c_itera = itera))
+
+            for itera in range(self.deck_ref['n_skip_per_c']):
+                self.deck.append(Card(c_type = "skip", c_color = color, c_value = self.points_ref["action_card_points"], c_itera = itera))
+
+            for itera in range(self.deck_ref['n_draw2_per_c']):
+                self.deck.append(Card(c_type = "draw2", c_color = color, c_value = self.points_ref["action_card_points"], c_itera = itera))
+
+            for itera in range(self.deck_ref['n_reverse_per_c']):
+                self.deck.append(Card(c_type = "reverse", c_color = color, c_value = self.points_ref["action_card_points"], c_itera = itera))
+
+        for itera in range(self.deck_ref['n_wild']):
+            self.deck.append(Card(c_type = "wild", c_color = color, c_value = self.points_ref["wild_card_points"], c_itera = itera))
+
+        for itera in range(self.deck_ref['n_wild4']):
+            self.deck.append(Card(c_type = "wild4", c_color = color, c_value = self.points_ref["wild_card_points"], c_itera = itera))
+
+        pass
+
+    def shuffle(self):
+        l = len(self.deck)
+        while l > 1:
+            i = int(floor(random() * l))
+            l -= 1
+            self.deck[i], self.deck[l] = self.deck[l], self.deck[i]
+        return self.deck
+
+    def draw(self):
+        pass
+
+    def discard(self):
+        pass
+
+    def assert_ruleset(self):
+        try:
+            assert (
+                self.deck_ref["n_cards"]
+                == (self.deck_ref["n_colors"] * self.deck_ref["n_cards_per_c"])
+                + self.deck_ref["n_wild"]
+                + self.deck_ref["n_wild4"]
+            )
+            assert (
+                self.deck_ref["n_cards_per_c"]
+                == self.deck_ref["n_zeros_per_c"]
+                + (self.deck_ref["n_1to9_per_c"] * 9)
+                + self.deck_ref["n_skip_per_c"]
+                + self.deck_ref["n_draw2_per_c"]
+                + self.deck_ref["n_reverse_per_c"]
+            )
+
+            assert self.deck_ref['n_cards'] <= 256
+
         except AssertionError:
             print("Invalid ruleset.")
 
-        for i in (range(self.deck_ref['n_zeros_per_c'] * self.deck_ref['n_colors'])):
-            deck.append(Card(c_type, c_color, self.ruleset))
+    def __str__(self):
+        return str([card.__str__() for card in self.deck])
 
-        
-        pass
-    def shuffle(self):
-        pass
-    def draw(self):
-        pass
-    def discard(self):
-        pass
-    def assert_ruleset(self):
-        assert (
-            self.deck_ref["n_cards"]
-            == (self.deck_ref["n_colors"] * self.deck_ref["n_cards_per_c"])
-            + self.deck_ref["n_wilds"]
-            + self.deck_ref["n_wildfours"]
-        )
-        assert (
-            self.deck_ref["n_cards_per_c"]
-            == self.deck_ref["n_zeros_per_c"]
-            + (self.deck_ref["n_1to9_per_c"] * 9)
-            + self.deck_ref["n_skip_per_c"]
-            + self.deck_ref["n_draw2_per_c"]
-            + self.deck_ref["n_reverse_per_c"]
-        )
-
-        assert self.deck_ref['n_cards'] <= 256
+    def __repr__(self):
+        return f"Deck({self.seed})"
 
 
 """ my_deck = Deck()
@@ -79,5 +146,7 @@ for i in range(0, 9):
         for c in self.deck_ref["n_colors"]: """
             
 
-
-print(Deck().deck_ref)
+deck = Deck()
+deck.shuffle()
+print(deck)
+#print(deck.deck_ref)
